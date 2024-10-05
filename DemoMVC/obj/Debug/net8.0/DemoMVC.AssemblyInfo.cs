12d514ee35ac3f6fb84aeeaa264549ceb4ccb388
HTTBAITHI2324 <hoangtin2003@gmail.com>
2024-10-05 14:43:15 +0700

    kethua

--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5d8a38c30c9fac2a2bc6b04bca6fd352ba8837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7f3aa2ccf021350fb45915ed44ef6f85de117f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
